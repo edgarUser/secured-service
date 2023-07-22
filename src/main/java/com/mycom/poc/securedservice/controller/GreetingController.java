d53554386dbddb49a1abf3b9ef5eab769fa9986a
edgarUser <edgar.lopez.isc@gmail.com>
2023-07-21 18:20:04 -0600

    Adding authorization default config

--- src/main/java/com/mycom/poc/securedservice/controller/GreetingController.java
+++ src/main/java/com/mycom/poc/securedservice/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.mycom.poc.securedservice.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,12 @@ public class GreetingController {
   @GetMapping("/greeting")
   public String greeting() {
     return "Hello world!";
+  }
+
+  @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+  @GetMapping("/greeting/{complement}")
+  public String greetingWithComplement(@PathVariable String complement) {
+    return String.format("Hello world %s!", complement);
   }
 
 }
